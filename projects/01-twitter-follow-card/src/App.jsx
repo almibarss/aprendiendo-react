e73db376b408327e29ee253b5123f24df4dc0034
almibarss <mperezibars@gmail.com>
2023-04-09 12:54:35 +0200

    Add project 01-twitter-follow-card

---
+++ projects/01-twitter-follow-card/src/App.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { TwitterFollowCard } from "./TwitterFollowCard";
+
+// These usually come from an API call
+const users = [
+    {
+        userName: "midudev",
+        name: "Miguel Angel Duran",
+        isFollowing: true,
+    },
+    {
+        userName: "pheralb",
+        name: "Pablo Heraldo",
+        isFollowing: false,
+    },
+    {
+        userName: "elonmusk",
+        name: "Elon Musk",
+        isFollowing: false,
+    },
+    {
+        userName: "vxnder",
+        name: "Vanderhart",
+        isFollowing: false,
+    },
+
+]
+
+export function App() {
+    return (
+        <section className="App">
+            {
+                users.map(({ userName, name, isFollowing }) => (
+                    <TwitterFollowCard
+                        userName={userName}
+                        name={name}
+                        initialIsFollowing={isFollowing}
+                    >
+                        {name}
+                    </TwitterFollowCard>
+                ))
+            }
+        </section>
+    )
+}
